d83fd720450e67989eb031603af30457cb724b19
Jonathan Rankin <jonathanrankin@ip-10-0-2-150.eu-west-1.compute.internal>
2023-11-02 10:04:44 +0200

    test

--- scripts.js
+++ scripts.js
@@ -23,6 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
         images[currentImageIndex].style.opacity = 1;
         dots[currentImageIndex].classList.add('active');
     }
-
+      //comments
     setInterval(changeImage, 6500); // Change image every 6.5 seconds to account for the longer transition
 });
